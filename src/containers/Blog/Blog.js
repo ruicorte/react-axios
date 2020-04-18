b3de0b166b784bd8460cbc1df0d958f6c47f67ad
ruicorte <ruicorte@gmail.com>
2020-04-18 16:30:48 +0100

    lazy loading new post page

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,18 +3,22 @@ import React, { Component } from 'react';
 
 // import Post from '../../components/Post/Post';
 // import FullPost from './FullPost/FullPost';
-import NewPost from './NewPost/NewPost';
-
+// import NewPost from './NewPost/NewPost';
 import './Blog.scss';
-
 import Posts from './Posts/Posts';
 // import FullPost from './FullPost/FullPost';
 
-import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
+
+// lazy loading / code splitting
+import asyncComponent from '../../hoc/asyncComponent';
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
     state = {
-        auth: false,
+        auth: true,
     };
 
     render() {
@@ -43,7 +47,8 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={() => <Posts />} /> */}
                 <Switch>
                     {/* <Route path="/new-post" component={NewPost} /> */}
-                    {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
+                    {/* {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null} */}
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null}
                     <Route path="/posts" component={Posts} />
                     <Route render={() => <h1>not found</h1>} />
                     {/* <Redirect from='/' to='/posts' /> */}
